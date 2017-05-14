43d7e76e45fa3d9c5c3cff666ca0778f6840d788
anke <245783065@qq.com>
2017-05-14 11:57:17 +0800

    Develop complete address page

--- js/address.js
+++ js/address.js
@@ -1,59 +1,44 @@
 new Vue({
-   el:'.container',
-    data:{
-       limitNum:3,
-        addressList:[],
-        currentIndex:0,
-        shippingMethod:1
+    el: ".container",
+    data: {
+        addressList: [],
+        limitNum: 3,
+        currentIndex: 0,
+        shippingMethod: 1
     },
-    mounted:function () {
+    mounted: function () {
         this.$nextTick(function () {
             this.getAddressList();
-        })
+        });
     },
-    methods:{
-       getAddressList:function () {
-           this.$http.get('data/address.json').then(response=>{
-               var res = response.data ;// 拿到json的数据
-               if(res.status=="0"){
-                   this.addressList = res.result;
-               }
-
-           })
-       },
-        setDefault(addressId){
-           this.addressList.map( ( address, index )=> {
-               if(address.addressId == addressId){
-                   address.isDefault = true;
-               }else {
-                   address.isDefault = false;
-               }
-           })
-
-        },
-        delProduct(index){
-            this.addressList.splice(index,1);
+    computed: {
+        filterAddress: function () {
+            return this.addressList.slice(0, this.limitNum);
         }
-        /*loadMore:function () {
-            this.limitNum = this.addressList.length;
-        }*/
     },
-    computed:{
-        filterAddress:function () {  // 过滤器 过滤数组
-            return this.addressList.slice(0,this.limitNum);// 使页面默认只显示 3条数据
+    methods: {
+        getAddressList: function () {
+            var _this = this;
+            this.$http.get("data/address.json").then(function (res) {
+                console.log(res);
+                var res = res.data;
+                if (res.status == '0') {
+                    _this.addressList = res.result;
+                }
+            });
+        },
+        loadMore: function () {
+            this.limitNum = this.addressList.length;
+        },
+        setDefault: function (addressId) {
+            this.addressList.forEach(function (address, index) {
+                if(address.addressId == addressId){
+                    address.isDefault = true;
+                }else{
+                    address.isDefault = false;
+                }
+            });
         }
     }
+
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
